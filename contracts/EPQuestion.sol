e0134f2cf52977271970470b73d3e9a0150dbdf5
Xiang Cui <xc1008@nyu.edu>
2022-02-25 14:13:09 -0500

    update

--- contracts/EPQuestion.sol
+++ contracts/EPQuestion.sol
@@ -9,7 +9,7 @@ contract EPQuestion is Ownable  {
 
     struct Question { 
         address owner;
-        address answerer;
+        bool active;
         uint256 delegateAmount;
     }
 
@@ -37,17 +37,19 @@ contract EPQuestion is Ownable  {
         stakingPercent = _stakingPercent;
     }
 
-    function adjustQuestionMintAmount(uint256 _questionMinAmount) public onlyOwner {
+    function adjustQuestionMinAmount(uint256 _questionMinAmount) public onlyOwner {
        questionMinAmount = _questionMinAmount;
-       emit parameterAdjusted("questionMinamount", _questionMinAmount);
+       emit parameterAdjusted("questionMinAmount", _questionMinAmount);
     }
 
     function adjustFeePercent(uint256 _feePercent) public onlyOwner {
         feePercent = _feePercent;
+        emit parameterAdjusted("feePercent", feePercent);
     }
 
     function adjustStakingPercent(uint256 _stakingPercent) public onlyOwner {
         stakingPercent = _stakingPercent;
+        emit parameterAdjusted("stakingPercent", stakingPercent);
     }
 
     function withdrawTeamFee() public onlyOwner {
@@ -62,29 +64,32 @@ contract EPQuestion is Ownable  {
         require(amount >= questionMinAmount, "minimum question fee required");
         token.transferFrom(msg.sender, address(this), amount);
         questionsInfo[id].owner = msg.sender;
-        questionsInfo[id].answerer = address(0);
+        questionsInfo[id].active = true;
         questionsInfo[id].delegateAmount = amount;
         emit questionCreated(id, amount);
     }
 
-    function closeQuestion(string memory id, address account) public {
+    function closeQuestion(string memory id, address[] memory account, uint256[] memory weight) public {
         require(questionsInfo[id].owner == msg.sender, 'invalid question creator');
-        require(questionsInfo[id].answerer == address(0), "Question closed");
-        questionsInfo[id].answerer = account;
+        require(questionsInfo[id].active, "Question closed");
+        questionsInfo[id].active = false;
         uint256 delegateAmount = questionsInfo[id].delegateAmount;
         token.approve(address(this), delegateAmount);
         uint256 teamFee = delegateAmount * feePercent / 100;
         cumulatedFee += teamFee;
         uint256 stakingReserved = delegateAmount * stakingPercent / 100;
         uint256 rewardAmount = delegateAmount - teamFee - stakingReserved;
-        token.transferFrom(address(this), account, rewardAmount);
+        for(uint i = 0; i < account.length; i++) {
+            uint256 userRewarded = weight[i] / rewardAmount * 100;
+            token.transferFrom(address(this), account[i], userRewarded);
+        }
         token.transferFrom(address(this), stakingPool, stakingReserved);
         token.approve(address(this), 0);
-        emit questionClosed(id, account);
+        emit questionClosed(id);
     }
 
     event parameterAdjusted(string name, uint256 amount);
     event questionCreated(string id, uint256 amount);
-    event questionClosed(string id, address account);
+    event questionClosed(string id);
 
 }
